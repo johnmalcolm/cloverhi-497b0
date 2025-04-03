e77114390a0720e1ffa440451a2f531c84aa5505
John Malcolm Anderson <john.anderson@netlight.com>
2025-04-03 10:32:48 +0300

    new meta

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,8 @@ import { Header } from '../components/header';
 
 export const metadata = {
     title: {
-        template: '%s | Netlify',
-        default: 'Netlify Starter'
+        template: '%s | Clover Hi',
+        default: 'Clover Hi | The Celtic Digital Agency'
     }
 };
 
